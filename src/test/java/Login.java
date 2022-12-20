73eb0b2ecfb0dadeb6e5d55fb480294cac8b5019
marinashamos <120131267+marinashamos@users.noreply.github.com>
2022-12-20 22:16:30 +0200

    QA36_Start
    create project 20.12.2022

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.beans.Transient;
+import java.util.List;
 
 public class Login {
 
@@ -12,10 +17,57 @@ public class Login {
         // open browser
         wd = new ChromeDriver();
         // open site
-        wd.get("https://telranedu.web.app/");
+        wd.get("https://telranedu.web.app");
     }
     @Test
+    public void findElementsOnPage() {
+        // by tagName
+        WebElement element = wd.findElement(By.tagName("div"));
+        List<WebElement> divs = wd.findElements(By.tagName("div"));
+
+        // By id
+        WebElement el1 = wd.findElement(By.id("root"));
+        // By class
+        WebElement el2 = wd.findElement(By.className("container"));
+        // By name
+        //wd.findElement(By.name("name"));
+
+        // By link text a href
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.linkText("ABOUT"));
+        wd.findElement(By.partialLinkText("HO"));
+
+    }
+
+    @Test
     public void loginSuccess(){
+
+        // open form
+        // wd find item + click
+
+        WebElement itemLogin = wd.findElement(By.linkText("LOGIN"));
+        itemLogin.click();
+
+        // fill form
+        // find email + click + clear + type
+        WebElement email = wd.findElement(By.name("email"));
+        email.click();
+        email.clear();
+        email.sendKeys("dsa+1@gmail.com");
+
+        // find password + click +clear + type
+        WebElement password = wd.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys("Qq12345$");
+
+        // submit  form
+        // find button login + click
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        WebElement buttonLogin = buttons.get(0);
+        buttonLogin.click();
+
+
         // open form
         // wd find button + click
         // fill form
@@ -25,9 +77,36 @@ public class Login {
         Assert.assertEquals("","");
         // ac == exp
     }
+
+    @Test
+    public void registrationSuccess(){
+
+        WebElement itemLogin = wd.findElement(By.linkText("LOGIN"));
+        itemLogin.click();
+
+        // fill form
+        // find email + click + clear + type
+        WebElement email = wd.findElement(By.name("email"));
+        email.click();
+        email.clear();
+        email.sendKeys("dsa+5@gmail.com");
+
+        // find password + click +clear + type
+        WebElement password = wd.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys("Qq123456$");
+
+        // submit  form
+        // find button login + click
+        List<WebElement> buttons = wd.findElements(By.tagName("button"));
+        WebElement buttonRegistration = buttons.get(1);
+        buttonRegistration.click();
+
+    }
     @BeforeMethod
     public void postCondition(){
         // close browser
-        wd.quit();
+        //wd.quit();
     }
 }
